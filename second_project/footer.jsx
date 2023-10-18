7bce010e2399cddfea9ab7fccfa9ce301fbe406d
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-10-18 16:55:43 +0800

    added: home page footer

---
+++ second_project/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <h1>Welcome to footer</h1>
+            </div>
+        </footer>
+    );
+}
